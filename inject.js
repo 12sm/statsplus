9457ac8dd12558e3a5daae90bded3a97c6c17222
Michael Shoup <michael@12southmusic.com>
2021-05-26 13:47:43 -0500

    Regex Update
    Updated the regex to support multiple currencies

--- inject.js
+++ inject.js
@@ -2,17 +2,26 @@
 
 (function() {
 
+//Quick function we'll use later for number cleanup
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 //Count up Transactions on page and make calculations
 var costs = document.querySelectorAll(".transaction-coin > span > span");
 var holding = document.querySelector(".pl-info > div > div > .info-item-value").textContent
 var netCosts = 0;
+//Regex to remove currency symbols and commas from the values so we can do math
+var regex = /[^0-9.]/g;
     for (var i = 0; i < costs.length; i++) {
-      netCosts += Number(costs[i].textContent.replace(/\$/g,''));
+      thisNum = Number(costs[i].textContent.replace(regex,''))
+      netCosts += parseFloat(thisNum);
 	}
 
 //Clean up those numbers to 2 decimal places
-var cleanCosts = Math.round(netCosts * 100) / 100;
-var costBasis = Math.round((netCosts/holding) * 100) / 100;
+
+var cleanCosts = numberWithCommas(netCosts.toFixed(2));
+var costBasis = numberWithCommas((netCosts/holding).toFixed(2));
 
 //Insert our calcs back into the page table
 var cloneBasis = document.querySelector(".pl-info").firstChild.lastChild.cloneNode(true);
